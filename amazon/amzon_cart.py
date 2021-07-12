261aaf64cc2873f3018ecee6ba8db91bbd598ebe
vigneshkumar <kumarmechdeveloper@gmail.com>
2021-07-12 15:59:06 +0530

    data driven test successfully done.

--- amazon/amzon_cart.py
+++ amazon/amzon_cart.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import selenium.webdriver.support.ui as ui
 import time
+import openpyxl
 
 "fire fox driver"
 # driver=webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
@@ -13,16 +14,37 @@ driver.get(url)
 driver.maximize_window()
 time.sleep(3)
 
+#configure spreadsheet path
+excel_path="/home/vigneshkumar/Documents/userdata.xlsx"
+
+b=openpyxl.load_workbook(excel_path)
+
+#open pyxl
+#get active sheet
+sheet=b.active
+#get cell address of email within active sheet
+email=sheet.cell(row=2,column=1)
+#get cell address of password withiin active sheet
+password=sheet.cell(row=2,column=2)
+#get values
+user_email=email.value
+user_password=password.value
+
+
 driver.find_element_by_xpath("//a[@id='nav-link-accountList']").click()
 time.sleep(3)
-driver.find_element_by_xpath("//input[@id='ap_email']").send_keys("7092030460")
+email=driver.find_element_by_xpath("//input[@id='ap_email']")
+email.send_keys(user_email)
 time.sleep(3)
-driver.find_element_by_xpath("//input[@id='continue']").click()
+conti=driver.find_element_by_xpath("//input[@id='continue']")
+conti.click()
 time.sleep(3)
-driver.find_element_by_xpath("//input[@id='ap_password']").send_keys("9525@amazon")
+pas_ssword=driver.find_element_by_xpath("//input[@id='ap_password']")
+pas_ssword.send_keys(user_password)
 time.sleep(3)
 driver.find_element_by_xpath("//input[@id='signInSubmit']").click()
 time.sleep(3)
+
 driver.find_element_by_xpath("//input[@id='twotabsearchtextbox']").click()
 driver.find_element_by_xpath("//input[@id='twotabsearchtextbox']").send_keys("i phone")
 driver.find_element_by_xpath("//input[@id='nav-search-submit-button']").click()
